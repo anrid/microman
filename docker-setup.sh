e2dfd3860fa5c5ba30a9ce70ad609777186e9f9c
anrid <anridig@gmail.com>
2017-07-26 01:19:38 +0700

    More!

--- docker-setup.sh
+++ docker-setup.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 docker volume create --name mongo-volume
 docker volume create --name elastic-volume
+docker volume create --name redis-volume
 # docker volume create --name mongo-volume --opt device=:/Users/anrid/dev/docker-data/mongo-volume
